f2bdc3c467a677a0a147aa8bfee3782859ac0aac
Isaac Martinez <imtz90b@gmail.com>
2019-08-26 16:50:08 -0600

    custom separator passing, negative number failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -9,7 +9,16 @@ namespace Kata
         {
             if (string.IsNullOrEmpty(number)) return 0;
 
-            var numbers = number.Split(new string[] {",", "\n"},
+            var separator = new string[] {",", "\n"};
+            var input = number;
+            if (number.StartsWith("//"))
+            {
+                var inputParts = number.Split("\n").ToArray();
+                separator = new [] {inputParts[0].Replace("//", "")};
+                input = inputParts[1];
+            }
+
+            var numbers = input.Split(separator,
                     StringSplitOptions.None)
                 .Select(int.Parse).ToArray();
             return numbers.Sum();
