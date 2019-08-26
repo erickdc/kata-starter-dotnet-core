6e2938d0306ed84eaebf788331888bc885d77003
Isaac Martinez <imtz90b@gmail.com>
2019-08-26 16:23:03 -0600

    one number passing, two numbers failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -4,7 +4,9 @@ namespace Kata
     {
         public int Add(string number)
         {
-            return 0;
+            if (string.IsNullOrEmpty(number))return 0;
+
+            return int.Parse(number);
         }
     }
 }
