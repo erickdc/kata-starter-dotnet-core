4af2b298dac4e205cdfe8ca5cbfa21727da5bf1e
Isaac Martinez <imtz90b@gmail.com>
2019-08-26 16:35:38 -0600

    multiple numbers passing newline delimiter failing

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -70,13 +70,26 @@ namespace Kata.Spec
         private static Calculator _systemUnderTest;
         private static int _result;
     }
+
+    public class when_delimiters_include_newline_and_comma
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("1,2\n3"); };
+
+        It should_split_and_sum_all_the_numbers = () => { _result.Should().Be(0); };
+        private static Calculator _systemUnderTest;
+        private static int _result;
+    }
 }
 
 
 /*
 
-3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
-4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
+
 5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
 6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
 7. Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
