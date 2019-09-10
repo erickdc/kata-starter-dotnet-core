99bfd0c6d18a388f08670053c1405f84112b5269
Isaac Martinez <imtz90b@gmail.com>
2019-09-10 16:29:14 -0600

    custom delimiter passing, one negative number failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -9,21 +9,28 @@ namespace Kata
         {
             if (string.IsNullOrEmpty(number))
             {
-                return 0;    
+                return 0;
             }
 
-            var delimeters = new String[]
+            var delimiters = new String[]
             {
                 ",",
                 "\n"
             };
-            var numbers = number.Split(delimeters, StringSplitOptions.None).Select(int.Parse).ToArray();
+            var numbersInput = number;
+            if (number.StartsWith("//"))
+            {
+                var inputParts = number.Split("\n");
+                delimiters = new[] {inputParts[0].Replace("//", "")};
+                numbersInput = inputParts[1];
+            }
+            var numbers = numbersInput.Split(delimiters, StringSplitOptions.None).Select(int.Parse).ToArray();
             if (numbers.Length > 1)
             {
                 return numbers.Sum();
             }
-            return numbers.First();
 
+            return numbers.First();
         }
     }
 }
