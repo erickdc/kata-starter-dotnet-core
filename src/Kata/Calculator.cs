74f2677bc04ecd26b9270e51723f9baa9c266e9f
Erick Caballero <erickdcb10@gmail.com>
2019-09-10 16:17:53 -0600

    adding spec for adding more than two numbers

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -9,7 +9,11 @@ namespace Kata
                 return 0;    
             }
 
-            
+            var numbers = number.Split(',');
+            if (numbers.Length > 1)
+            {
+                return  int.Parse(numbers[0]) + int.Parse(numbers[1]);
+            }
             return int.Parse(number);
 
         }
