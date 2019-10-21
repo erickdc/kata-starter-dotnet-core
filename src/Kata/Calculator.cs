758b5e787aa7e5d1dd91ad9de851ce81a756353a
Erick Caballero <erickdcb10@gmail.com>
2019-10-21 11:27:19 -0600

    adding multiple delimter

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -16,7 +16,7 @@ namespace Kata
                 separators = new[] {parts[0].Replace("//", "")};
                 s = parts[1];
             }
-            var numbers = s.Split(separators, StringSplitOptions.None).Select(int.Parse);
+            var numbers = s.Split(separators, StringSplitOptions.None).Select(int.Parse).Where(n => n < 1000);
             var negativeNumbers = numbers.Where(n => n < 0);
             if (negativeNumbers.Any())
             {
