cd728a69674c9d34dc2233bfdcdde6f9a5239661
Erick Caballero <erickdcb10@gmail.com>
2019-10-28 11:58:58 -0600

    refactor

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Kata
@@ -9,20 +10,34 @@ namespace Kata
         {
             if(string.IsNullOrEmpty(s))
                 return 0;
-            var delimeters = new [] { ",", "\n"};
+            var delimeters = GetDelimeters(ref s);
+            var numbers = getNumbers(s, delimeters);
+            return numbers.Sum();
+        }
+
+        private static string[] GetDelimeters(ref string s)
+        {
+            var delimeters = new[] {",", "\n"};
             if (s.StartsWith("//"))
             {
                 var parts = s.Split("\n");
-                delimeters = new[] {parts[0].Replace("//", "").Replace("[","").Replace("]","")};
+                delimeters = parts[0].Replace("//", "").Replace("[", "").Split("]");
                 s = parts[1];
             }
+
+            return delimeters;
+        }
+
+        private static IEnumerable<int> getNumbers(string s, string[] delimeters)
+        {
             var numbers = s.Split(delimeters, StringSplitOptions.None).Select(int.Parse).Where(n => n < 1001);
             var negativeNumbers = numbers.Where(n => n < 0);
             if (negativeNumbers.Count() > 0)
             {
-                 throw new Exception($"negatives not allowed: {string.Join(", ", negativeNumbers)}");
+                throw new Exception($"negatives not allowed: {string.Join(", ", negativeNumbers)}");
             }
-            return numbers.Sum();
+
+            return numbers;
         }
     }
 }
