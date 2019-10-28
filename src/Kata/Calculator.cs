7dd8b216525b0df7cb67f3408360b980d2afe6da
csolorzanof <carlos.solorzano@acklenavenue.com>
2019-10-28 11:48:27 -0600

    custom delimiter passing, negative failinf

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -10,6 +10,12 @@ namespace Kata
             if(string.IsNullOrEmpty(s))
                 return 0;
             var delimeters = new [] { ",", "\n"};
+            if (s.StartsWith("//"))
+            {
+                var parts = s.Split("\n");
+                delimeters = new[] {parts[0].Replace("//", "")};
+                s = parts[1];
+            }
             var numbers = s.Split(delimeters, StringSplitOptions.None).Select(int.Parse);
             return numbers.Sum();
         }
