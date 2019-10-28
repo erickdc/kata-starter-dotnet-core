ec168f868e7c20539fadb4bc875c1654827b45d9
Erick Caballero <erickdcb10@gmail.com>
2019-10-28 11:41:38 -0600

    adding more than 2 num

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -59,6 +59,19 @@ namespace Kata.Spec
         static int _result;
     }
 
+    public class when_adding_more_than_two_number
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("1,2,3"); };
+        It should_return_the_sum = () => { _result.Should().Be(6); };
+        private static Calculator _systemUnderTest;
+        private static int _result;
+    }
+
     //3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
     //4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
     //5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace Kata
 {
     public class Calculator
@@ -6,7 +8,13 @@ namespace Kata
         {
             if(string.IsNullOrEmpty(s))
                 return 0;
-            return int.Parse(s);
+            var numbers = s.Split(",").Select(int.Parse);
+            if (numbers.Count() == 1)
+            {
+                return numbers.First();
+            }
+            var enumerable = numbers.ToList();
+            return enumerable.First() + enumerable.Last();
         }
     }
 }
